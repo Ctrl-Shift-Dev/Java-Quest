d6054cab9a5403a914610578d7c657e52f8dd574
Andre_Neves <64751008+andreN4vs@users.noreply.github.com>
2024-05-08 08:37:45 -0300

    connection update

--- src/main/java/factory/ConnectionFactory.java
+++ src/main/java/factory/ConnectionFactory.java
@@ -7,14 +7,61 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public Connection getConnection(){
+    private String url;
+    private String user;
+    private String password;
+    private Connection connection;
 
-        try{
-            Dotenv dotenv = Dotenv.load();
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/world","root", dotenv.get("SQL_PASSWORD"));
+    public ConnectionFactory(String url, String user, String password) {
+        this.url = "jdbc:mysql://localhost:3306/world";
+        this.user = "root";
+        this.password = "root";
+    }
+
+    public void connect() {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            if (connection != null) {
+                System.out.println("Conexão bem-sucedida!");
+            } else {
+                System.out.println("Falha ao conectar ao banco de dados.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro de SQL: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver JDBC não encontrado.");
         }
-        catch (SQLException exception){
-            throw new RuntimeException(exception);
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Conexão fechada.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao fechar a conexão: " + e.getMessage());
         }
+    }
+
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/world";
+        String user = "root";
+        String password = "fatec";
+
+        ConnectionFactory connector = new ConnectionFactory(url, user, password);
+        connector.connect();
+        Connection connection = connector.getConnection();
+
+        connector.close();
     }
 }
--- src/main/java/factory/ConnectionTest.java
+++ src/main/java/factory/ConnectionTest.java
@@ -1,15 +1,15 @@
-package factory;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public class ConnectionTest {
-
-    public static void main(String[] args) throws SQLException {
-
-        Connection connection = new ConnectionFactory().getConnection();
-        System.out.println("Conexão aberta!");
-        connection.close();
-
-    }
-}
+//package factory;
+//
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//public class ConnectionTest {
+//
+//    public static void main(String[] args) throws SQLException {
+//
+//        Connection connection = ConnectionFactory.getConnection();
+//        System.out.println("Conexão aberta!");
+//        connection.close();
+//
+//    }
+//}
